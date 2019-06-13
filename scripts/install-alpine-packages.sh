89c23b5fa3adb2d6228457cfb8941d911d4d7d49
Leonid Meleshin <leon.03.99@gmail.com>
2019-06-13 19:39:43 +0300

    feat: add essential extensions

--- scripts/install-alpine-packages.sh
+++ scripts/install-alpine-packages.sh
@@ -1,7 +1,53 @@
 #!/bin/sh
-set -e
+
+set -eu
+set -o pipefail
 
 apk add -Uuv \
-    git bash supervisor freetype-dev libjpeg-turbo-dev libzip-dev \
-    libpng-dev postgresql-dev nginx \
-    && rm -rf /var/cache/apk/*
+    git \
+    bash \
+    nginx
+
+apk add -Uuv --virtual .build-dependencies \
+    $PHPIZE_DEPS \
+    zlib-dev \
+    cyrus-sasl-dev \
+    git \
+    autoconf \
+    g++ \
+    libtool \
+    make \
+    pcre-dev
+
+# intl, zip, soap
+echo "Installing intl, zip, soap"
+apk add -Uuv \
+    libintl \
+    icu \
+    icu-dev \
+    libxml2-dev
+
+# mysqli, pdo, pdo_mysql, pdo_pgsql
+echo "Installing mysqli, pdo, pdo_mysql, pdo_pgsql"
+apk add -Uuv postgresql-dev
+
+# mcrypt, gd, iconv
+echo "Installing mcrypt, gd, iconv"
+apk add -Uuv \
+    freetype-dev \
+    libjpeg-turbo-dev \
+    libpng-dev \
+    libmcrypt-dev \
+    libzip-dev \
+
+# gmp
+echo "Installing gmp"
+apk add -Uuv \
+    gmp \
+    gmp-dev
+
+# imagick
+# echo "Installing imagick"
+# apk add -Uuv autoconf g++ imagemagick-dev libtool make pcre-dev
+    
+rm -rf /var/cache/apk/*
--- scripts/install-extensions.sh
+++ scripts/install-extensions.sh
@@ -1,4 +1,34 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+set -eu
+set -o pipefail
+
+pecl install apcu
+# docker-php-ext-install -j"$(getconf _NPROCESSORS_ONLN)" apcu
+
+# intl, zip, soap
+docker-php-ext-install -j"$(getconf _NPROCESSORS_ONLN)" intl zip soap
+
+# mysqli, pdo, pdo_mysql, pdo_pgsql
+docker-php-ext-install -j"$(getconf _NPROCESSORS_ONLN)" pdo pdo_mysql pdo_pgsql mysqli
+
+# mcrypt, gd, iconv
+pecl install mcrypt-1.0.2
+docker-php-ext-enable mcrypt
+docker-php-ext-install -j"$(getconf _NPROCESSORS_ONLN)" iconv
+docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ 
+docker-php-ext-install -j"$(getconf _NPROCESSORS_ONLN)" gd
+
+# gmp
+apk add --update --no-cache gmp gmp-dev \
+    && docker-php-ext-install gmp
+
+# imagick
+# pecl install imagick \
+# docker-php-ext-enable imagick
+
+# bcmath
+docker-php-ext-install bcmath
 
 # Download phpredis sources to a dir that docker-php-ext-install will look in and make it aware it's there.
 export PHPREDIS_VERSION="${PHPREDIS_VERSION-4.2.0}"
@@ -6,6 +36,5 @@ mkdir -p /usr/src/php/ext/redis \
     && curl -L https://github.com/phpredis/phpredis/archive/$PHPREDIS_VERSION.tar.gz | tar xvz -C /usr/src/php/ext/redis --strip 1 \
     && echo 'redis' >> /usr/src/php-available-exts
 
-# Install mysql, mysqli, opcache, phpredis, and pcntl
-NPROC=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || 1)
-docker-php-ext-install "-j${NPROC}" pdo pdo_mysql pdo_pgsql mysqli opcache redis pcntl gd bcmath posix zip
+docker-php-ext-configure opcache --enable-opcache
+docker-php-ext-install -j"$(getconf _NPROCESSORS_ONLN)" opcache redis pcntl posix
